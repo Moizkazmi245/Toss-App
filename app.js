cee2d6b6436d4bc4aeceb499b510e6f4806fea57
Moizkazmi245 <abdul.moiz.kazmi@gmail.com>
2024-09-29 21:59:02 +0500

    ab done

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ function tossCoin() {
       
       if (random < 0.5) {
         result.innerHTML = 'Heads!';
-        coin.style.backgroundImage = "url('51xs7F+tP5L._AC_UF894,1000_QL80_.jpg')";
+        coin.style.backgroundImage = "url('heads.jpg')";
       } else {
         result.innerHTML = 'Tails!';
-        coin.style.backgroundImage = "url('51bcZy+HZpL._AC_UF894,1000_QL80_.jpg')";
+        coin.style.backgroundImage = "url('tails.jpg')";
       }
       
       
