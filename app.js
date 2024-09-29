53bd286949d35107e143c880c4e7cc3d6ca6f704
Moizkazmi245 <abdul.moiz.kazmi@gmail.com>
2024-09-29 22:01:47 +0500

    done

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
-var coin =  document.getElementById('coin');
+var coin = document.getElementById('coin');
 var result = document.getElementById('result');
 
 
 
 
 function tossCoin() {
-   coin.style.animation = 'spin 2s ease-in-out';  
-    result.innerHTML = ''; 
-    setTimeout(function() {
-     
-      const random = Math.random();
-      
-      
-      if (random < 0.5) {
-        result.innerHTML = 'Heads!';
-        coin.style.backgroundImage = "url('heads.jpg')";
-      } else {
-        result.innerHTML = 'Tails!';
-        coin.style.backgroundImage = "url('tails.jpg')";
-      }
-      
-      
-     coin.style.animation = 'none';
-    }, 2000);  
-  }
-  
+    coin.style.animation = 'spin 2s ease-in-out';
+    result.innerHTML = '';
+    setTimeout(function () {
+
+        const random = Math.random();
+
+
+        if (random < 0.5) {
+            result.innerHTML = 'Heads!';
+            coin.style.backgroundImage = "url('heads.jpg')";
+        } else {
+            result.innerHTML = 'Tails!';
+            coin.style.backgroundImage = "url('tails.jpg')";
+        }
+
+
+        coin.style.animation = 'none';
+    }, 2000);
+}
